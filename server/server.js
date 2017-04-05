726253926e6dea94e3d71ebaa06e146c12ea7369
Mike Tobo <tobo@Mikes-MBP.lan>
2017-04-05 17:04:09 -0400

    Added GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const
     express = require("express"),
-    bodyParser = require("body-parser");
+    bodyParser = require("body-parser"),
+    {ObjectId} = require("mongodb");
 
 const
     {mongoose} = require("./db/mongoose"),
@@ -28,6 +29,25 @@ app.get("/todos", (req, res) => {
         res.send({todos});
     }, (err) => {
         res.status(400).send(err);
+    });
+});
+
+app.get("/todos/:id", (req, res) =>{
+    const id = req.params.id;
+
+    if(!ObjectId.isValid(id)){
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+
+    }).catch((err) => {
+        res.status(400).send();
     });
 });
 
