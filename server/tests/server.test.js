9c0d6edd8c751ce4d42bb9a8b5bf97856ac48c33
Mike Tobo <tobo@Mikes-MBP.lan>
2017-04-09 21:19:43 -0400

    Added tests for DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -112,3 +112,41 @@ describe("GET /todos/:id", () => {
     });
 
 });
+
+describe("DELETE /todos/:id", () => {
+    it("should remove a todo", (done) => {
+        var hexId = todos[1]._id.toHexString();
+
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(hexId);
+            })
+            .end((err, res) => {
+                if(err){
+                    return done(err);
+                }
+
+                Todo.findById(hexId).then((todo) => {
+                    expect(todo).toNotExist();
+                    done();
+                }).catch((err) => done(err));
+            });
+    });
+
+    it("should return 404 if todo not found", (done) => {
+        request(app)
+            .delete(`/todos/${new ObjectId().toHexString()}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it("should return 404 if ObjectId is invalid", (done) => {
+        request(app)
+            .delete('/todos/123')
+            .expect(404)
+            .end(done);
+    });
+
+});
